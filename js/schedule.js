4ccd9313eddb346851ff670c0978295e008b6ae1
Luca Scuderi <luca.scuderi@me.com>
2022-01-25 14:40:55 +0100

    Initial commit

---
+++ js/schedule.js
@@ -0,0 +1,105 @@
+new Vue({
+	el: '#schedule',
+	data: {
+		day1: [
+			{
+				time: '09:00',
+				emoji: '☕️',
+				title: 'Welcome Coffee',
+				room: 'Atrium',
+			},
+			{
+				time: '09:30',
+				emoji: '🎤',
+				title: 'Workshops Presentations',
+				room: 'Auditorium',
+			},
+			{
+				time: '10:00',
+				emoji: '💻',
+				title: 'Session 1',
+				room: 'Auditorium',
+			},
+			{
+				time: '12:00',
+				emoji: '🍝',
+				title: 'Lunch Break',
+				room: 'Auditorium',
+			},
+			{
+				time: '13:00',
+				emoji: '💻',
+				title: 'Session 2',
+				room: 'Auditorium',
+			},
+			{
+				time: '15:00',
+				emoji: '☕️',
+				title: 'Coffee Break',
+				room: 'Auditorium',
+			},
+			{
+				time: '15:30',
+				emoji: '💻',
+				title: 'Session 3',
+				room: 'Auditorium',
+			},
+			{
+				time: '20:30',
+				emoji: '🎉',
+				title: 'Party',
+				room: 'Auditorium',
+			},
+		],
+		day2: [
+			{
+				time: '09:00',
+				emoji: '☕️',
+				title: 'Welcome Coffee',
+				room: 'Atrium',
+			},
+			{
+				time: '09:30',
+				emoji: '🎤',
+				title: 'Workshops Presentations',
+				room: 'Auditorium',
+			},
+			{
+				time: '10:00',
+				emoji: '💻',
+				title: 'Session 1',
+				room: 'Auditorium',
+			},
+			{
+				time: '12:00',
+				emoji: '🍝',
+				title: 'Lunch Break',
+				room: 'Auditorium',
+			},
+			{
+				time: '13:00',
+				emoji: '💻',
+				title: 'Session 2',
+				room: 'Auditorium',
+			},
+			{
+				time: '15:00',
+				emoji: '☕️',
+				title: 'Coffee Break',
+				room: 'Auditorium',
+			},
+			{
+				time: '15:30',
+				emoji: '💻',
+				title: 'Session 3',
+				room: 'Auditorium',
+			},
+			{
+				time: '20:30',
+				emoji: '👋',
+				title: 'Closing',
+				room: 'Auditorium',
+			},
+		],
+	},
+});
---
+++ js/speakers.js
@@ -0,0 +1,31 @@
+new Vue({
+	el: '#mentors',
+	data: {
+		speakers: [
+			{
+				firstName: 'Patrice',
+				lastName: 'Müller',
+				jobTitle: 'Android Lead Engineer',
+				company: 'Migros Online',
+				picture: 'img/avatar.svg',
+				bio: 'Short bio describing the mentor and eventually her/his workshop',
+			},
+			{
+				firstName: 'Sebastiano',
+				lastName: 'Poggi',
+				jobTitle: '-',
+				company: 'Jetbrains',
+				picture: 'img/avatar.svg',
+				bio: 'Short bio describing the mentor and eventually her/his workshop',
+			},
+			{
+				firstName: 'Ivan ',
+				lastName: 'Morgillo',
+				jobTitle: '-',
+				company: 'Jetbrains',
+				picture: 'img/avatar.svg',
+				bio: 'Short bio describing the mentor and eventually her/his workshop',
+			},
+		],
+	},
+});
