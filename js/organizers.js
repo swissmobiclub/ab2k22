d2313b930cbe7eed2a1f6850506e3e34bcb9b8f7
Luca Scuderi <luca.scuderi@me.com>
2022-01-30 16:06:46 +0100

    Add venue & organizers information

---
+++ js/organizers.js
@@ -0,0 +1,31 @@
+new Vue({
+	el: '#about',
+	data: {
+		organizers: [
+			{
+				firstName: 'Samuel',
+				lastName: 'Bichsel',
+				jobTitle: '',
+				company: 'dreipol',
+				picture: 'img/avatar.svg',
+			},
+			{
+				firstName: 'Seán',
+				lastName: 'Labastille',
+				jobTitle: '',
+				company: 'Fairtiq',
+				picture: 'img/avatar.svg',
+				twitter: 'flufffel',
+			},
+			{
+				firstName: 'Luca',
+				lastName: 'Scuderi',
+				jobTitle: 'iOS Engineer',
+				company: 'Migros Online',
+				picture: 'img/avatar.svg',
+				twitter: 'lucascuderi',
+				github: 'lscuderi',
+			},
+		],
+	},
+});
