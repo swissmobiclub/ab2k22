654941c96e9cdc27b3ed3582dc606c9676d62d61
Luca Scuderi <luca.scuderi@me.com>
2022-02-08 18:04:45 +0100

    Add first mentors data and tweaks

---
+++ js/mentors.js
@@ -0,0 +1,28 @@
+new Vue({
+	el: '#mentors',
+	data: {
+		mentors: [
+			{
+				firstName: 'Sebastiano',
+				lastName: 'Poggi',
+				jobTitle: 'Team Lead',
+				company: 'JetBrains',
+				picture: 'img/people/sebastiano.jpg',
+				twitter: 'seebrock3r',
+				website: 'https://sebastiano.dev',
+				bio: 'Emerging from the foggy plains of northern Italy 🇮🇹 after working at a smartwatch startup, Sebastiano moved with his curls to London 🇬🇧 💂 to do great things at AKQA and then Novoda. In the meantime, he\'s become a Google Developer Expert and a frequent speaker at conferences. Nowadays he\'s working remotely for JetBrains and is in Italy once again 🇮🇹. He really wishes he had more time to write technical articles on his blog, but he\'s live coding on Twitch in the meantime on <a target="_blank" href="http://codewiththeitalians.it">codewiththeitalians.it</a>',
+			},
+			{
+				firstName: 'Ivan',
+				lastName: 'Morgillo',
+				jobTitle: 'Android Consultant and Trainer',
+				company: 'JetBrains',
+				picture: 'img/people/ivan.jpg',
+				twitter: 'hamen',
+				github: 'hamen',
+				linkedin: 'ivanmorgillo',
+				bio: 'I\'m a passionate Android developer, a writer, a speaker, a trainer, and I\'m always looking for new opportunities to learn and share back my findings with the community. I\'m also 1/2 of <a target="_blank" href="http://codewiththeitalians.it">codewiththeitalians.it</a> 😁',
+			},
+		],
+	},
+});
